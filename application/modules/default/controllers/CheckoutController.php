82ff8225a164fb0b70a6722db18eebc915ecacf6
Okalany Daniel <dokalanyi@gmail.com>
2015-01-05 16:54:55 +0300

    Order separation for EFT

--- application/modules/default/controllers/CheckoutController.php
+++ application/modules/default/controllers/CheckoutController.php
@@ -37,9 +37,9 @@ class CheckoutController extends App_Controller_Action
         if (!$this->cart['user'])
             $this->model->updateItem(array('user' => $this->_auth->UniqueRef), $this->cart['id']);
         
-        $items = $this->view->items = $this->getCartItems();
+        $i = $this->view->items = $this->getCartItems();
 
-        if (!$items || empty($items))
+        if (!$i || empty($i))
             $this->_helper->redirector('index');
         
         $form    = new Default_Form_Payment();
@@ -61,88 +61,80 @@ class CheckoutController extends App_Controller_Action
                     $this->_helper->redirector->gotoRoute(array('reference' => $reference), 'checkout-paygate');
                     
                 } else {
-                
                     $paytypes = $form->getPaymentOptions();
                     
-                    $params = array(
-                        'AccountUniqueRef' => $this->_auth->UniqueRef,
-                        'PaymentMethot'    => $paytypes[$form->getValue('paytype')],
-                        'OrderState'       => 'Unpaid',
-                    );
-                    
-                    $order = $this->_api->createOrder($params);
-                    
-                    if (true === $order->Status) {
+                    $sortedItems = array();
+                    foreach ($i as $cartitem) {
+                        $sortedItems[$cartitem["student_name"]][] = $cartitem;
+                    }
+                    //Zend_Debug::dump($sortedItems);
+                    //exit();
+                    $orders = "";
+                    foreach ($sortedItems as $items) {
+                        $params = array(
+                            'AccountUniqueRef' => $this->_auth->UniqueRef,
+                            'PaymentMethot'    => $paytypes[$form->getValue('paytype')],
+                            'OrderState'       => 'Unpaid',
+                        );
 
-                        foreach ($items as $item) {
-                        
-                            $params = array(
-                                'OrderRef'  => $order->OrderRef,
-                                'Quantity'  => $item['qty'],
-                            );
-                        
-                            if ('stock' === $item['type']) {
-                            
-                                $params['StockID'] = $item['sid'];
-                                
-                            } elseif ('book' === $item['type']) {
-                                
-                                $params['ISBN'] = $item['sid'];
-                                
-                            }
-                             $params['Total'] = $item['total'] ;
-				$params['GradeID'] = $item['gradeid'] ;
+                        $order = $this->_api->createOrder($params);
 
-                            // specific for bundle items
+                        if (true === $order->Status) {
+                            $orders .= $order->OrderRef . "|";
+                            foreach ($items as $item) {
+                                $params = array(
+                                    'OrderRef'  => $order->OrderRef,
+                                    'Quantity'  => $item['qty'],
+                                );
                             
-                            if ($item['bundle_id']) {
-                                $params['PriceList'] = $item['bundle_id'];
-                                $params['StudentUniqueRef'] = $item['student_id'];
-                            }
-
-                            // end
-                            
-                            $result = $this->_api->createOrderItem($params);
-                            
-                            if (true !== $result->Status) {
-                                
-                                if ('development' === APPLICATION_ENV) {
-                                    Zend_Debug::dump($params);
-                                    Zend_Debug::dump($result);
-                                    exit;
+                                if ('stock' === $item['type']) {
+                                    $params['StockID'] = $item['sid'];
+                                } elseif ('book' === $item['type']) {
+                                    $params['ISBN'] = $item['sid'];
                                 }
+                                $params['Total'] = $item['total'];
+                                $params['GradeID'] = $item['gradeid'] ;
+
+                                // specific for bundle items
                                 
-                                // delete the order (need a function)
-                                goto error;
+                                if ($item['bundle_id']) {
+                                    $params['PriceList'] = $item['bundle_id'];
+                                    $params['StudentUniqueRef'] = $item['student_id'];
+                                }
+
+                                // end
                                 
+                                $result = $this->_api->createOrderItem($params);
+                                
+                                if (true !== $result->Status) {
+                                    
+                                    if ('development' === APPLICATION_ENV) {
+                                        Zend_Debug::dump($params);
+                                        Zend_Debug::dump($result);
+                                        exit;
+                                    }
+                                    
+                                    // delete the order (need a function)
+                                    $this->_helper->flashMessenger(array('error' => $this->view->translate('An unknown error has occurred. Please contact us for further assistance.')));
+                                }
                             }
-                            
-                           // Zend_Debug::dump($params);
-                           // Zend_Debug::dump($result);
-                            
+                        } else {
+                            if ('development' === APPLICATION_ENV) {
+                                Zend_Debug::dump($params);
+                                Zend_Debug::dump($order);
+                                exit();
+                            }
                         }
-                        
-                        //exit;
-                        
-                        $this->session->unsetAll();
-                        $this->model->deleteItem($this->cart['id']);
-                        
-                        $this->_helper->redirector->gotoRoute(array('reference' => $order->OrderRef), 'checkout-complete');
-                        
-                    } 
-                    
-                    error:
-                    $this->_helper->flashMessenger(array('error' => $this->view->translate('An unknown error has occurred. Please contact us for further assistance.')));
-                    
+                    }
+
+                    //exit;
+                    $this->session->unsetAll();
+                    $this->model->deleteItem($this->cart['id']);
+                    $this->_helper->redirector->gotoRoute(array('reference' => $orders), 'checkout-complete');
                 }
-                
-                
             } else {
-                
                 $this->_helper->flashMessenger(array('error' => $this->view->translate('Please select a payment option before proceeding.')));
-                
             }
-            
         }
         
         $this->view->cart = $this->cart;
@@ -151,21 +143,40 @@ class CheckoutController extends App_Controller_Action
 
     public function completeAction()
     {
-        $result = $this->_api->getOrderDetails($this->getParam('reference', null));
+        $orders = $this->getParam('reference', null);
+        $orders = split("\|", $orders);
+
+        $results = array();
+        foreach ($orders as $OrderRef) {
+            if(empty($OrderRef)) {
+                continue;
+            }
+            $result = $this->_api->getOrderDetails($OrderRef);
+            if (true === $result->Status)
+                $results[] = $result;
+        }
         
-        if (true === $result->Status)
+        if (!empty($results)) {
+            $result = $results[0];
+            $sum = 0;
+            foreach ($results as $res) {
+                $sum += $res->_TotalAmount;
+            }
+
+            $result->_TotalAmount = $sum;
             $this->view->order = $result;
-    
+        }
+        //if (true === $result->Status)
+            //$this->view->order = $result;
         // Zend_Debug::dump($result);
     }
     
     private function getCartItems()
     {
         $model = new App_Model_CartItem();
-             
-	 $form    = new Default_Form_Payment();
-	  $this->view->form = $form;
-  	return $model->getByCart($this->cart['id']);
+        $form = new Default_Form_Payment();
+        $this->view->form = $form;
+        return $model->getByCart($this->cart['id']);
     }
     
 }
