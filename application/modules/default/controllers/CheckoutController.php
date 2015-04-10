a975262dc1769cef4bc1c35b4f250d325a3b09db
Okalany Daniel <dokalanyi@gmail.com>
2015-04-10 10:35:09 +0300

    Added cloudbanc

--- application/modules/default/controllers/CheckoutController.php
+++ application/modules/default/controllers/CheckoutController.php
@@ -46,95 +46,16 @@ class CheckoutController extends App_Controller_Action
         $request = $this->getRequest();
 
         if ($request->isPost()) {
-            
             $data = $request->getPost();
-            
-            if ($form->isValid($data)) {
-            
-                $paytype = $form->getValue('paytype');
-                
-                if ($paytype === 'card') {
-                    
-                    $reference = strtoupper(uniqid(uniqid(), true));
-                    $this->model->updateItem(array('reference' => $reference), $this->cart['id']);
-                    
-                    $this->_helper->redirector->gotoRoute(array('reference' => $reference), 'checkout-paygate');
-                    
-                } else {
-                    $paytypes = $form->getPaymentOptions();
-                    
-                    $sortedItems = array();
-                    foreach ($i as $cartitem) {
-                        $sortedItems[$cartitem["student_name"]][] = $cartitem;
-                    }
-                    //Zend_Debug::dump($sortedItems);
-                    //exit();
-                    $orders = "";
-                    foreach ($sortedItems as $items) {
-                        $params = array(
-                            'AccountUniqueRef' => $this->_auth->UniqueRef,
-                            'PaymentMethot'    => $paytypes[$form->getValue('paytype')],
-                            'OrderState'       => 'Unpaid',
-                        );
+            $url = "http://41.185.31.134:81/requests.aspx?verb=getpaymentcodeforcompanycode&param1=1WY5PJSJ&param2=92";
+            $client = new Zend_Http_Client($url, array('timeout' => 300));
+            $token_info = $client->request()->getBody();
 
-                        $order = $this->_api->createOrder($params);
-
-                        if (true === $order->Status) {
-                            $orders .= $order->OrderRef . "|";
-                            foreach ($items as $item) {
-                                $params = array(
-                                    'OrderRef'  => $order->OrderRef,
-                                    'Quantity'  => $item['qty'],
-                                );
-                            
-                                if ('stock' === $item['type']) {
-                                    $params['StockID'] = $item['sid'];
-                                } elseif ('book' === $item['type']) {
-                                    $params['ISBN'] = $item['sid'];
-                                }
-                                $params['Total'] = $item['total'];
-                                $params['GradeID'] = $item['gradeid'] ;
-
-                                // specific for bundle items
-                                
-                                if ($item['bundle_id']) {
-                                    $params['PriceList'] = $item['bundle_id'];
-                                    $params['StudentUniqueRef'] = $item['student_id'];
-                                }
-
-                                // end
-                                
-                                $result = $this->_api->createOrderItem($params);
-                                
-                                if (true !== $result->Status) {
-                                    
-                                    if ('development' === APPLICATION_ENV) {
-                                        Zend_Debug::dump($params);
-                                        Zend_Debug::dump($result);
-                                        exit;
-                                    }
-                                    
-                                    // delete the order (need a function)
-                                    $this->_helper->flashMessenger(array('error' => $this->view->translate('An unknown error has occurred. Please contact us for further assistance.')));
-                                }
-                            }
-                        } else {
-                            if ('development' === APPLICATION_ENV) {
-                                Zend_Debug::dump($params);
-                                Zend_Debug::dump($order);
-                                exit();
-                            }
-                        }
-                    }
-
-                    //exit;
-                    $this->session->unsetAll();
-                    $this->model->deleteItem($this->cart['id']);
-                    $this->_helper->redirector->gotoRoute(array('reference' => $orders), 'checkout-complete');
-                }
-            } else {
-                $this->_helper->flashMessenger(array('error' => $this->view->translate('Please select a payment option before proceeding.')));
-            }
+            $token = split("\|", $token_info);
+            $token = split("\*", $token[1]);
+            $token = $token[1];
+            $this->_helper->redirector->gotourl("http://wallet.cloudbanc.co.za/code/" . $token);
+            exit();
         }
         
         $this->view->cart = $this->cart;
@@ -168,7 +89,7 @@ class CheckoutController extends App_Controller_Action
         }
         //if (true === $result->Status)
             //$this->view->order = $result;
-        // Zend_Debug::dump($result);
+        Zend_Debug::dump($results);
     }
     
     private function getCartItems()
